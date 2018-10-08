8104500b017fe6b738121b9ad5d71074f23d790e
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2018-10-07 22:24:41 -0400

    Attempt fix for leaving unjoined voice channel error message

--- cmd-voice.go
+++ cmd-voice.go
@@ -22,7 +22,7 @@ func commandVoiceJoin(args []string, env *CommandEnvironment) *discordgo.Message
 
 func commandVoiceLeave(args []string, env *CommandEnvironment) *discordgo.MessageEmbed {
 	for _, voiceState := range env.Guild.VoiceStates {
-		if guildData[env.Guild.ID].VoiceData.VoiceConnection.ChannelID == "" {
+		if guildData[env.Guild.ID].VoiceData.VoiceConnection == nil {
 			return NewErrorEmbed("Voice Error", botData.BotName+" is not currently in a voice channel.")
 		}
 		if voiceState.UserID == env.Message.Author.ID && voiceState.ChannelID == guildData[env.Guild.ID].VoiceData.VoiceConnection.ChannelID {
