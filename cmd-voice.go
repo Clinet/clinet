c5d0872b4be2a0e83c290c524c55df54ca1fb18d
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2018-11-02 21:41:29 -0400

    Add error handling for invalid Member pointer when creating queue entries

--- cmd-voice.go
+++ cmd-voice.go
@@ -125,6 +125,9 @@ func commandPlay(args []string, env *CommandEnvironment) *discordgo.MessageEmbed
 		if err != nil {
 			return NewErrorEmbed("Voice Error", "There was an error finding a service to handle the specified URL.")
 		}
+		if env.Member != nil {
+			return NewErrorEmbed("Voice Error", "There was an error figuring out who requested the track.")
+		}
 		queueEntry.Requester = env.Member.User
 		go voicePlayWrapper(botData.DiscordSession, env.Guild.ID, env.Channel.ID, queueEntry, true)
 		return nil
