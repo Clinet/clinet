7f13a7c2f808ddb6a8bd871cff300eb55a75d7fa
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2018-11-29 01:25:38 -0500

    Add RegexBadMatch expression to NLP, add queue list NLP, add queue list page number to NLP, add more NLP error handling, and break out of NLP testing instead of continuing loop if no matches are found

--- nlp.go
+++ nlp.go
@@ -15,6 +15,8 @@ type CommandNLP struct {
 // NLP holds data related to a query command executable by any message system
 //
 // Command and Regex must always be specified.
+// If RegexBadMatch is specified, it will be used for the process of checking for bad matches. If the
+// subject message matches this expression, the NLP handler will bail.
 // If RegexArguments is specified, it will be used for the process of pulling arguments.
 // If RegexReplace is specified, it will be used for the process of ordering and modifying the arguments.
 // Once a match succeeds and a string of arguments is pulled, it will then be split by spaces and passed
@@ -29,20 +31,23 @@ type NLP struct {
 	ArgPrefix      string         //The prefix to apply to pulled arguments
 	ArgSuffix      string         //The suffix to apply to pulled arguments
 	Regex          *regexp.Regexp //The regular expression to match against
+	RegexBadMatch  *regexp.Regexp //The regular expression to test bad matches against
 	RegexArguments *regexp.Regexp //The regular expression to pull arguments with
 	RegexReplace   string         //The replacement scheme to follow for pulling arguments
 }
 
 func initNLPCommands() {
-	addNLPCommand(nlpNew("spotify", "search", "", regexp.MustCompile("(?i)(?:.*?)(?:play|listen to)(?:\\s)(.*)(?:\\s)(?:from Spotify|on Spotify)(?:.*)"), nil, "${1}"),
-		nlpNew("spotify", "play", "", regexp.MustCompile("(?i)(?:.*?)(?:play|listen to)(?:\\s)(.*)(?:\\s)(?:from Spotify|on Spotify)(?:.*)"), nil, "1"))
-	addNLPCommand(nlpNew("play", "", "", regexp.MustCompile("(?i)(?:.*?)(?:play|listen to)(?:\\s)(.*)"), nil, ""))
-	addNLPCommand(nlpNew("queue", "remove", "", regexp.MustCompile("(?i)(?:.*?)(?:.*?)(?:remove|delete)(?:.*)(\\b\\d+)(?:.*)(?:queue)(?:.*)"), regexp.MustCompile("(\\b\\d+)"), ""))
-	addNLPCommand(nlpNew("remind", "", "", regexp.MustCompile("(?i)(.*?)(?:.*?)(remind me|set a reminder)(.*)"), nil, ""))
+	addNLPCommand(nlpNew("spotify", "search", "", regexp.MustCompile("(?i)(?:.*?)(?:play|listen to)(?:\\s)(.*)(?:\\s)(?:from Spotify|on Spotify)(?:.*)"), nil, nil, "${1}"),
+		nlpNew("spotify", "play", "", regexp.MustCompile("(?i)(?:.*?)(?:play|listen to)(?:\\s)(.*)(?:\\s)(?:from Spotify|on Spotify)(?:.*)"), nil, nil, "1"))
+	addNLPCommand(nlpNew("play", "", "", regexp.MustCompile("(?i)(?:.*?)(?:play|listen to)(?:\\s)(.*)"), nil, nil, ""))
+	addNLPCommand(nlpNew("queue", "", "", regexp.MustCompile("(?i)(?:.*?)(\\d+)(?:.*?)(?:queue)(?:.*?)"), regexp.MustCompile("(?i)(?:.*?)(?:remove|delete)(?:.*?)"), regexp.MustCompile("(\\d+)"), ""))
+	addNLPCommand(nlpNew("queue", "", "", regexp.MustCompile("(?i)(?:.*?)(?:queue)(?:.*)"), regexp.MustCompile("(?i)(?:.*?)(?:remove|delete)(?:.*?)"), nil, "${1}"))
+	addNLPCommand(nlpNew("queue", "remove", "", regexp.MustCompile("(?i)(?:.*?)(?:remove|delete)(?:.*)(\\b\\d+)(?:.*)(?:queue)(?:.*)"), regexp.MustCompile("(\\b\\d+)"), nil, ""))
+	addNLPCommand(nlpNew("remind", "", "", regexp.MustCompile("(?i)(.*?)(?:.*?)(remind me|set a reminder)(.*)"), nil, nil, ""))
 }
 
-func nlpNew(command, argPrefix, argSuffix string, regex, regexArguments *regexp.Regexp, regexReplace string) *NLP {
-	return &NLP{Command: command, ArgPrefix: argPrefix, ArgSuffix: argSuffix, Regex: regex, RegexArguments: regexArguments, RegexReplace: regexReplace}
+func nlpNew(command, argPrefix, argSuffix string, regex, regexBadMatch, regexArguments *regexp.Regexp, regexReplace string) *NLP {
+	return &NLP{Command: command, ArgPrefix: argPrefix, ArgSuffix: argSuffix, Regex: regex, RegexBadMatch: regexBadMatch, RegexArguments: regexArguments, RegexReplace: regexReplace}
 }
 
 func addNLPCommand(nlp ...*NLP) {
@@ -50,49 +55,75 @@ func addNLPCommand(nlp ...*NLP) {
 }
 
 func callNLP(message string, env *CommandEnvironment) *discordgo.MessageEmbed {
-	for _, command := range botData.NLPCommands {
-		for i := 0; i < len(command.Commands); i++ {
-			nlp := command.Commands[i]
+	for i, command := range botData.NLPCommands {
+		for j := 0; j < len(command.Commands); j++ {
+			Debug.Printf("Testing NLP %d, command %d...", i, j)
+
+			nlp := command.Commands[j]
+
+			if nlp.Regex == nil {
+				break
+			}
 
 			if !nlp.Regex.MatchString(message) {
-				continue
+				break
+			}
+
+			if nlp.RegexBadMatch != nil {
+				if nlp.RegexBadMatch.MatchString(message) {
+					break
+				}
 			}
 
 			if _, exists := botData.Commands[nlp.Command]; !exists {
-				continue
+				break
 			}
 
 			env.Command = nlp.Command
 
 			matches := make([]string, 0)
 			if nlp.RegexArguments != nil {
+				Debug.Println("RegexArguments: True")
 				if nlp.RegexReplace != "" {
+					Debug.Println("RegexReplace: True")
 					replaced := nlp.RegexArguments.ReplaceAllString(message, nlp.RegexReplace)
 					matches = strings.Split(replaced, " ")
 				} else {
+					Debug.Println("RegexReplace: False")
 					matches = nlp.RegexArguments.FindAllString(message, -1)
 				}
 			} else {
+				Debug.Println("RegexArguments: False")
 				if nlp.RegexReplace != "" {
+					Debug.Println("RegexReplace: True")
 					replaced := nlp.Regex.ReplaceAllString(message, nlp.RegexReplace)
 					matches = strings.Split(replaced, " ")
 				} else {
+					Debug.Println("RegexReplace: False")
+					Debug.Println(nlp.RegexArguments)
+					Debug.Println(nlp.RegexReplace)
 					matches = nlp.Regex.FindAllString(message, -1)
 				}
 			}
 
 			if nlp.ArgPrefix != "" {
+				Debug.Println("Prefix:", nlp.ArgPrefix)
 				prefixes := strings.Split(nlp.ArgPrefix, " ")
 				matches = append(prefixes, matches...)
 			}
 			if nlp.ArgSuffix != "" {
+				Debug.Println("Suffix:", nlp.ArgSuffix)
 				suffixes := strings.Split(nlp.ArgSuffix, " ")
 				matches = append(matches, suffixes...)
 			}
-			Debug.Println("Matches:", matches)
+			Debug.Printf("Matches (%d): %v", len(matches), matches)
+
+			if len(matches) == 1 && matches[0] == "" {
+				matches = nil
+			}
 
 			embed := callCommand(nlp.Command, matches, env)
-			if i == (len(command.Commands) - 1) {
+			if j == (len(command.Commands) - 1) {
 				return embed
 			} else {
 				if strings.Contains(embed.Title, "Error") {
