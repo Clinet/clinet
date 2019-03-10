3d2f4fcf3c8b9b6df93d0057b625c1871e14a5cc
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2019-03-10 00:24:20 -0500

    Add format code handling to JSON-based Minecraft server descriptions and fix issue where obfuscation format code didn't reset when a new format code was encountered

--- cmd-minecraft.go
+++ cmd-minecraft.go
@@ -12,7 +12,7 @@ import (
 	"4d63.com/tz"
 	"github.com/Syfaro/minepong"
 	"github.com/bwmarrin/discordgo"
-	"github.com/dustin/go-humanize"
+	humanize "github.com/dustin/go-humanize"
 	"github.com/minotar/minecraft"
 )
 
@@ -180,6 +180,7 @@ func commandMinecraft(args []string, env *CommandEnvironment) *discordgo.Message
 			SetTitle(title).
 			AddField("Version", version).
 			AddField("Players", strconv.Itoa(server.Players.Online)+"/"+strconv.Itoa(server.Players.Max)).
+			AddField("Description", mcFormat(server.Description)).
 			SetColor(0x5B8731)
 
 		if server.FavIcon != "" {
@@ -187,149 +188,160 @@ func commandMinecraft(args []string, env *CommandEnvironment) *discordgo.Message
 			minecraftEmbed.SetThumbnail("https://api.minetools.eu/favicon/" + hostReplaced)
 		}
 
-		switch v := server.Description.(type) {
-		case map[string]interface{}:
-			jsonData, err := json.Marshal(server.Description)
-			if err == nil {
-				var advancedDescription *MCServerDescription
-				if err := json.Unmarshal(jsonData, &advancedDescription); err == nil {
-					if len(advancedDescription.Extra) > 0 {
-						formattedDescription := ""
-						for _, extra := range advancedDescription.Extra {
-							text := strings.TrimPrefix(strings.TrimSuffix(extra.Text, " "), " ")
-							if extra.Bold {
-								text = "**" + text + "**"
-							}
-							if extra.Strikethrough {
-								text = "~~" + text + "~~"
-							}
-							if extra.Underline {
-								text = "__" + text + "__"
-							}
-							if extra.Italic {
-								text = "*" + text + "*"
-							}
-							formattedDescription += text + " "
-						}
-						minecraftEmbed.AddField("Description", formattedDescription)
-					} else if advancedDescription.Text != "" {
-						minecraftEmbed.AddField("Description", advancedDescription.Text)
-					}
-				}
-			}
-		case string:
-			if v != "" {
-				format := false
-				obfuscated := false
-				bold := false
-				strikethrough := false
-				underline := false
-				italic := false
-
-				formattedDescription := ""
-				for _, character := range v {
-					switch string(character) {
-					case "§":
-						format = true
-						continue
-					case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f":
-						if format {
-							format = false
-							continue
-						}
-					case "k":
-						if format {
-							format = false
-							obfuscated = true
-							continue
-						}
-					case "l":
-						if format {
-							format = false
-							if bold == false {
-								bold = true
-								formattedDescription += "**"
-							}
-							continue
-						}
-					case "m":
-						if format {
-							format = false
-							if strikethrough == false {
-								strikethrough = true
-								formattedDescription += "~~"
-							}
-							continue
-						}
-					case "n":
-						if format {
-							format = false
-							if underline == false {
-								underline = true
-								formattedDescription += "__"
-							}
-							continue
-						}
-					case "o":
-						if format {
-							format = false
-							if italic == false {
-								italic = true
-								formattedDescription += "*"
-							}
-							continue
-						}
-					case "r":
-						if format {
-							format = false
-							obfuscated = false
-							if bold {
-								bold = false
-								formattedDescription += "**"
-							}
-							if strikethrough {
-								strikethrough = false
-								formattedDescription += "~~"
-							}
-							if underline {
-								underline = false
-								formattedDescription += "__"
-							}
-							if italic {
-								italic = false
-								formattedDescription += "*"
-							}
-							continue
-						}
-					}
-					if obfuscated {
-						formattedDescription += "▓"
-					} else {
-						formattedDescription += string(character)
-					}
-				}
-
-				if bold {
-					formattedDescription += "**"
-				}
-				if strikethrough {
-					formattedDescription += "~~"
-				}
-				if underline {
-					formattedDescription += "__"
-				}
-				if italic {
-					formattedDescription += "*"
-				}
-
-				minecraftEmbed.AddField("Description", formattedDescription)
-			}
-		default:
-			minecraftEmbed.AddField("Unknown Type", fmt.Sprintf("%T", v))
-		}
-
 		return minecraftEmbed.MessageEmbed
 	}
 
 	return NewErrorEmbed("Minecraft Error", "Unknown command ``"+args[1]+"``.")
+}
+
+func mcFormat(desc interface{}) string {
+	switch v := desc.(type) {
+	case map[string]interface{}:
+		jsonData, err := json.Marshal(desc)
+		if err == nil {
+			var advancedDescription *MCServerDescription
+			if err := json.Unmarshal(jsonData, &advancedDescription); err == nil {
+				if len(advancedDescription.Extra) > 0 {
+					formattedDescription := ""
+					for _, extra := range advancedDescription.Extra {
+						text := strings.TrimPrefix(strings.TrimSuffix(extra.Text, " "), " ")
+						if extra.Bold {
+							text = "**" + text + "**"
+						}
+						if extra.Strikethrough {
+							text = "~~" + text + "~~"
+						}
+						if extra.Underline {
+							text = "__" + text + "__"
+						}
+						if extra.Italic {
+							text = "*" + text + "*"
+						}
+						formattedDescription += text + " "
+					}
+					return mcFormat(formattedDescription)
+				} else if advancedDescription.Text != "" {
+					return mcFormat(advancedDescription.Text)
+				}
+			}
+		}
+	case string:
+		if v != "" {
+			format := false
+			obfuscated := false
+			bold := false
+			strikethrough := false
+			underline := false
+			italic := false
+
+			formattedDescription := ""
+			for _, character := range v {
+				switch string(character) {
+				case "§":
+					format = true
+					obfuscated = false
+					continue
+				case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f":
+					if format {
+						format = false
+						continue
+					}
+				case "k":
+					if format {
+						format = false
+						obfuscated = true
+						continue
+					}
+				case "l":
+					if format {
+						format = false
+						if bold == false {
+							bold = true
+							formattedDescription += "**"
+						}
+						continue
+					}
+				case "m":
+					if format {
+						format = false
+						if strikethrough == false {
+							strikethrough = true
+							formattedDescription += "~~"
+						}
+						continue
+					}
+				case "n":
+					if format {
+						format = false
+						if underline == false {
+							underline = true
+							formattedDescription += "__"
+						}
+						continue
+					}
+				case "o":
+					if format {
+						format = false
+						if italic == false {
+							italic = true
+							formattedDescription += "*"
+						}
+						continue
+					}
+				case "r":
+					if format {
+						format = false
+						if bold {
+							bold = false
+							formattedDescription += "**"
+						}
+						if strikethrough {
+							strikethrough = false
+							formattedDescription += "~~"
+						}
+						if underline {
+							underline = false
+							formattedDescription += "__"
+						}
+						if italic {
+							italic = false
+							formattedDescription += "*"
+						}
+						continue
+					}
+				case "\\":
+					formattedDescription += "\\"
+					continue
+				case "*":
+					formattedDescription += "\\*"
+				case "_":
+					formattedDescription += "\\_"
+				case "~":
+					formattedDescription += "\\~"
+				}
+				if obfuscated {
+					formattedDescription += "▓"
+				} else {
+					formattedDescription += string(character)
+				}
+			}
+
+			if bold {
+				formattedDescription += "**"
+			}
+			if strikethrough {
+				formattedDescription += "~~"
+			}
+			if underline {
+				formattedDescription += "__"
+			}
+			if italic {
+				formattedDescription += "*"
+			}
+
+			return formattedDescription
+		}
+	}
+
+	return ""
 }
