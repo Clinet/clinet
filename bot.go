65a9063824c2e048f8618fc10ad91159ccefaa65
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2022-11-21 04:02:41 -0500

    clinet: Add essentials feature and fix template

--- bot.go
+++ bot.go
@@ -16,6 +16,7 @@ import (
 
 	//Clinet's features
 	"github.com/Clinet/clinet_features_dumpctx"
+	"github.com/Clinet/clinet_features_essentials"
 	"github.com/Clinet/clinet_features_hellodolly"
 	"github.com/Clinet/clinet_features_moderation"
 	"github.com/Clinet/clinet_features_voice"
@@ -37,53 +38,62 @@ func doBot() {
 	//For some reason we don't automatically exit as planned when we return to main()
 	defer os.Exit(0)
 
-	log.Info("Loading configuration...")
-	cfg, err := config.LoadConfig(configFile, config.ConfigTypeJSON)
+	log.Info("Loading features...")
+	cfg, err := config.LoadConfig(featuresFile, config.ConfigTypeJSON)
 	if err != nil {
-		log.Error("Error loading configuration: ", err)
+		log.Error("Error loading features: ", err)
 		return
 	}
 
-	log.Info("Syncing configuration...")
-	cfg.SaveTo(configFile, config.ConfigTypeJSON)
-
-	if writeConfigTemplate {
-		log.Info("Updating configuration template...")
-		templateCfg := config.NewConfig()
-		templateCfg.Features = append(templateCfg.Features, &features.Feature{Name: "example", Toggle: true})
-		templateCfg.SaveTo("config.template.json", config.ConfigTypeJSON)
+	if cfg.Features == nil || len(cfg.Features) == 0 {
+		log.Error("No features found in configuration!")
+		return
 	}
 
+	log.Debug("Syncing format of features file...")
+	cfg.SaveTo(featuresFile, config.ConfigTypeJSON)
+
+	//Initialize Clinet using the configuration provided
+	log.Info("Initializing instance of Clinet...")
 	clinet = bot.NewBot(cfg)
+
+	//Clinet is effectively online at this stage, so defer shutdown in case of errors below
 	defer clinet.Shutdown()
 
+	//Register the conversation services to handle queries
+	log.Debug("Registering conversation services...")
+	log.Trace("- duckduckgo")
+	logFatalError(clinet.RegisterFeature(duckduckgo.Feature))
+	log.Trace("- wolframalpha")
+	logFatalError(clinet.RegisterFeature(wolframalpha.Feature))
+
+	//Register the features to handle commands
 	log.Debug("Registering features...")
-	if err := clinet.RegisterFeature(dumpctx.Feature); err != nil {
-		log.Fatal(err)
-	}
-	if err := clinet.RegisterFeature(hellodolly.Feature); err != nil {
-		log.Fatal(err)
-	}
-	if err := clinet.RegisterFeature(moderation.Feature); err != nil {
-		log.Fatal(err)
-	}
-	if err := clinet.RegisterFeature(voice.Feature); err != nil {
-		log.Fatal(err)
+	log.Trace("- dumpctx")
+	logFatalError(clinet.RegisterFeature(dumpctx.Feature))
+	log.Trace("- hellodolly")
+	logFatalError(clinet.RegisterFeature(hellodolly.Feature))
+	log.Trace("- moderation")
+	logFatalError(clinet.RegisterFeature(moderation.Feature))
+	log.Trace("- voice")
+	logFatalError(clinet.RegisterFeature(voice.Feature))
+	log.Trace("- essentials")
+	logFatalError(clinet.RegisterFeature(essentials.Feature)) //ALWAYS REGISTER ESSENTIALS LAST BEFORE CHAT SERVICES!
+
+	log.Debug("Registering chat services...")
+	log.Trace("- discord")
+	logFatalError(clinet.RegisterFeature(discord.Feature))
+	log.Trace("- guilded")
+	logFatalError(clinet.RegisterFeature(guilded.Feature))
+
+	if writeFeaturesTemplate {
+		log.Debug("Updating features template...")
+		templateCfg := config.NewConfig()
+		templateCfg.Features = features.FM.Features
+		templateCfg.SaveTo("features.template.json", config.ConfigTypeJSON)
 	}
 
-	if err := clinet.RegisterConvoService("duckduckgo", duckduckgo.DuckDuckGo); err != nil {
-		log.Fatal(err)
-	}
-	if err := clinet.RegisterConvoService("wolframalpha", wolframalpha.WolframAlpha); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := clinet.RegisterService("discord", discord.Discord); err != nil {
-		log.Fatal(err)
-	}
-	if err := clinet.RegisterService("guilded", guilded.Guilded); err != nil {
-		log.Fatal(err)
-	}
+	log.Info("Clinet is now online!")
 
 	log.Debug("Waiting for SIGINT syscall signal...")
 	sc := make(chan os.Signal, 1)
@@ -91,4 +101,10 @@ func doBot() {
 	<-sc
 
 	log.Info("Good-bye!")
+}
+
+func logFatalError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- main.go
+++ main.go
@@ -7,22 +7,22 @@ import (
 
 var (
 	//Various command-line flags
-	configFile          string //path to bot configuration
-	writeConfigTemplate bool   //if true, write the current configuration template to config.template.json
-	verbosity           int    //0 = default (info, warning, error), 1 = 0 + debug, 2 = 1 + trace
+	featuresFile          string //path to bot feature toggles
+	writeFeaturesTemplate bool   //if true, write the current features template to features.template.json
+	verbosity             int    //0 = default (info, warning, error), 1 = 0 + debug, 2 = 1 + trace
 )
 
 func init() {
 	watchdog.Header = "Clinet © JoshuaDoes 2017-2022."
 	watchdog.Footer = "Good-bye!"
-	watchdog.ImmediateSpawn = false //We want to display Header for a short time
+	watchdog.ImmediateSpawn = true //We want to display Header for a short time
 	watchdog.KillOldMain = true //We want to be the only live instance of this bot
 }
 
 func main() {
 	//Apply all command-line flags
-	pflag.StringVar(&configFile, "config", "config.json", "path to bot configuration")
-	pflag.BoolVar(&writeConfigTemplate, "writeConfigTemplate", true, "write the current configuration template to config.template.json")
+	pflag.StringVar(&featuresFile, "features", "features.json", "path to bot feature toggles")
+	pflag.BoolVar(&writeFeaturesTemplate, "writeFeaturesTemplate", true, "write the current features template to features.template.json")
 	pflag.IntVar(&verbosity, "verbosity", 0, "sets the verbosity level; 0 = default, 1 = debug, 2 = trace")
 
 	if watchdog.Parse() {
